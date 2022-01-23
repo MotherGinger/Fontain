05e04dbd9bce6cd77a8940f58c6544648ee39cda
MotherGinger <29235654+MotherGinger@users.noreply.github.com>
2022-01-23 02:00:11 -0500

    Fix single font override menu

--- Fontain/Fontain.lua
+++ Fontain/Fontain.lua
@@ -30,16 +30,18 @@ function mod:OnInitialize()
 	self.options.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
 	self.options.args.profile.order = 101
 	self:ApplyFonts()
-	
+
 	UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT = 12
 	CHAT_FONT_HEIGHTS = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
 end
 
 function mod:ADDON_LOADED()
-	if not db then return end	
-	UNIT_NAME_FONT     = media:Fetch("font", db.fonts["UnitNames"]) or UNIT_NAME_FONT
-	NAMEPLATE_FONT     = media:Fetch("font", db.fonts["Nameplates"]) or NAMEPLATE_FONT
-	DAMAGE_TEXT_FONT   = media:Fetch("font", db.fonts["DamageText"]) or DAMAGE_TEXT_FONT
+	if not db then
+		return
+	end
+	UNIT_NAME_FONT = media:Fetch("font", db.fonts["UnitNames"]) or UNIT_NAME_FONT
+	NAMEPLATE_FONT = media:Fetch("font", db.fonts["Nameplates"]) or NAMEPLATE_FONT
+	DAMAGE_TEXT_FONT = media:Fetch("font", db.fonts["DamageText"]) or DAMAGE_TEXT_FONT
 	STANDARD_TEXT_FONT = media:Fetch("font", db.fonts["StandardText"]) or STANDARD_TEXT_FONT
 end
 mod:RegisterEvent("ADDON_LOADED")
@@ -49,40 +51,44 @@ function mod:OnEnable()
 		self:Disable()
 		return
 	end
-	
+
 	if db.override then
 		media:SetGlobal("font", db.font)
-	end	
-	
+	end
+
 	media.RegisterCallback(mod, "LibSharedMedia_Registered")
 	media.RegisterCallback(mod, "LibSharedMedia_SetGlobal", "ApplyFonts")
 	self:LibSharedMedia_Registered()
-	
+
 	for k, v in pairs(_G) do
 		-- pcall needed to protect against tables with metatables.
 		if type(v) == "table" then
 			local meta = getmetatable(v)
-			local _, _, objType = pcall(v.GetObjectType,v)
-			if meta and meta.__index and type(meta.__index) == "table" and meta.__index.GetFont and objType == "Font" then
+			local _, objType = pcall(v.GetObjectType, v)
+			if meta and type(meta) == "table" and meta.__index and type(meta.__index) == "table" and meta.__index.GetFont and objType == "Font" then
 				tinsert(fontList, v)
 			end
 		end
 	end
-	table.sort(fontList, function(a, b) return #(a:GetName()) > #(b:GetName()) end)
+	table.sort(
+		fontList,
+		function(a, b)
+			return #(a:GetName()) > #(b:GetName())
+		end
+	)
 	self:BuildFontMenus()
 	self:ApplyFonts()
-	
+
 	self.db.RegisterCallback(self, "OnProfileChanged", "ProfileChanged")
 	self.db.RegisterCallback(self, "OnProfileCopied", "ProfileChanged")
 	self.db.RegisterCallback(self, "OnProfileReset", "ProfileChanged")
-	
 end
 
 function mod:ProfileChanged()
 	db = self.db.profile
 	if db.override then
 		media:SetGlobal("font", db.font)
-	end	
+	end
 	self:RevertFonts()
 	self:ApplyFonts()
 end
@@ -94,37 +100,43 @@ function mod:OnDisable()
 
 	media.UnregisterCallback(mod, "LibSharedMedia_Registered")
 	media:SetGlobal("font", nil)
-	self:RevertFonts()	
+	self:RevertFonts()
 end
 
 do
 	local fonts, sizes, flags, NORMAL
-	
+
 	local function setFont(f, key)
 		local n = f:GetName()
+		key = key or n
+
 		if not fontDefaults[n] then
 			fontDefaults[n] = {f:GetFont()}
 		end
-		key = key or n
-		f:SetFont(key and fonts[key] and media:Fetch("font", fonts[key]) or NORMAL, sizes[key] or fontDefaults[n][2], flags[key] or fontDefaults[n][3])
+
+		if not key == "FontainExample" then
+			f:SetFont(key and fonts[key] and media:Fetch("font", fonts[key]) or NORMAL, sizes[key] or fontDefaults[n][2], flags[key] or fontDefaults[n][3])
+		end
 	end
 
 	function mod:ApplyFonts(fontName)
-		if not self:IsEnabled() then return end
+		if not self:IsEnabled() then
+			return
+		end
 		fonts = db.fonts
 		sizes = db.sizes
 		flags = db.flags
 		NORMAL = media:Fetch("font", db.font)
-		
+
 		self:ADDON_LOADED()
-		
+
 		if fontName then
 			for _, f in ipairs(fontList) do
 				if f:GetName():match("^" .. fontName) then
 					setFont(f, fontName)
 				end
 			end
-		else			
+		else
 			for _, f in ipairs(fontList) do
 				local n = f:GetName()
 				local keyMatch
@@ -143,7 +155,7 @@ end
 function mod:Dump()
 	db.defaults = {}
 	db.replaced = {}
-	
+
 	db.defaults = fontDefaults
 	for _, f in ipairs(fontList) do
 		local n = f:GetName()
